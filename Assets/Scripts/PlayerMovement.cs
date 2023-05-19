f141d5144de90a8eb0ccc07c0626d27c0fc76e3f
GoktugVar <goktuguum@gmail.com>
2023-05-19 22:34:24 +0300

    .

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,9 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] float m_speed = 4.0f;
     [SerializeField] float m_jumpForce = 7.5f;
-    public float pressTime=0;
-    private bool canFly=false;
-    public float healt=100;
+    private float pressTime = 0;
+    private bool canFly;
+    private float healt = 100;
     public float m_healt
     {
         get
@@ -32,8 +32,9 @@ public class PlayerMovement : MonoBehaviour
     private bool m_grounded = false;
     private float m_delayToIdle = 0.0f;
     private float inputX;
-    public float flySpeed=10;
-    
+    private float flySpeed = 16;
+
+
     void Start()
     {
         m_animator = GetComponent<Animator>();
@@ -50,7 +51,6 @@ public class PlayerMovement : MonoBehaviour
         {
             ChangeHealt(20);
         }
-        // Grounded and jump thing
         if (!m_grounded && m_groundSensor.State())
         {
             m_grounded = true;
@@ -58,13 +58,12 @@ public class PlayerMovement : MonoBehaviour
         }
         else if (m_grounded && !m_groundSensor.State())
         {
-            StartCoroutine(ResetGroundedState(0.3f)); // wait for 0.3 sec before resetting grounded state
+            StartCoroutine(ResetGroundedState(0.3f));
         }
 
         // -- Handle input and movement --
         inputX = Input.GetAxis("Horizontal");
 
-        // Swap direction of sprite depending on walk direction
         if (inputX > 0)
         {
             GetComponent<SpriteRenderer>().flipX = false;
@@ -74,9 +73,9 @@ public class PlayerMovement : MonoBehaviour
             GetComponent<SpriteRenderer>().flipX = true;
         }
 
-        //Set AirSpeed in animator
         m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
 
+       
         // -- Handle Animations --
 
         //Death
@@ -84,34 +83,39 @@ public class PlayerMovement : MonoBehaviour
         {
             m_animator.SetTrigger("Death");
         }
-
         //Jump
-        if (Input.GetKeyDown(KeyCode.Space) && m_grounded)
+        if (Input.GetKeyDown(KeyCode.W) && m_grounded)
         {
-            
             m_animator.SetTrigger("Jump");
             m_grounded = false;
             m_animator.SetBool("Grounded", m_grounded);
             m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
             m_groundSensor.Disable(0.2f);
         }
-        //Fly
-       if(Input.GetKeyDown(KeyCode.Z) && m_grounded){
-                canFly=true;
-                pressTime=Time.time;
+        //Fly Start
+        if (Input.GetKeyDown(KeyCode.Space) && m_grounded)
+        {
+            canFly = true;
+            pressTime = Time.time;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && canFly && Time.time - pressTime <= 1)
+        {
+            Fly();
+        }
+        else
+        {
+            canFly = false;
         }
         //Run
         if (Mathf.Abs(inputX) > Mathf.Epsilon)
         {
-            // Reset timer
             m_delayToIdle = 0.05f;
             m_animator.SetInteger("AnimState", 1);
         }
-
         //Idle
         else
         {
-            // Prevents flickering transitions to idle
             m_delayToIdle -= Time.deltaTime;
             if (m_delayToIdle < 0)
                 m_animator.SetInteger("AnimState", 0);
@@ -125,24 +129,12 @@ public class PlayerMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
-       
         m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
-  
-        
-    if(Input.GetKey(KeyCode.Z) && canFly && Time.time-pressTime<=1){
-        Fly();
-        
-        
-        }
-        else{
-            canFly=false;
-        }
-       
     }
 
-    public void  Fly () {
-        
-        m_body2d.AddForce(Vector2.up*flySpeed);
+    public void Fly()
+    {
+        m_body2d.AddForce(Vector2.up * flySpeed);
     }
 
     public void ChangeHealt(int x)
@@ -154,12 +146,11 @@ public class PlayerMovement : MonoBehaviour
         else if( m_healt+x <= 0) 
         {
             m_healt = 0;
-            // ölme fonskiyonu
+            // ölme fonskiyonu buraya gelecek
         }
         else
         {
             m_healt += x;
         }
-
     }
 }
