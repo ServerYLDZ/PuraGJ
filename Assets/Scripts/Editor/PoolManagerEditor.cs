248aeaf7ccbfd4a52a20fd5bf7495c360f60c15a
GoktugVar <goktuguum@gmail.com>
2023-05-20 15:27:07 +0300

    velet

---
+++ Assets/Scripts/Editor/PoolManagerEditor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(PoolManager))]
+public class PoolManagerEditor : Editor {
+
+	PoolManager pool;
+	void OnEnable(){
+		pool = (PoolManager)target;
+	}
+	public override void OnInspectorGUI () {
+
+		GUILayout.Space (10);
+		GUILayout.BeginVertical ("Box");
+		GUILayout.Label ("Pool Manager");
+		GUILayout.BeginHorizontal();
+		GUILayout.Label ("Object in Pool: "+pool.objectPrefabs.Count.ToString());
+		pool.defaultBufferAmount= (int)EditorGUILayout.IntField("Default Amount:",pool.defaultBufferAmount, GUILayout.MinWidth(100));
+		GUILayout.EndHorizontal();
+		GUILayout.Space (10);
+		GUILayout.EndVertical ();
+		GUILayout.Space (10);
+		if (GUILayout.Button ("Add Object to Pool")) {
+			addObject();
+		}
+		GUILayout.Space (10);
+		for (int cnt=0; cnt<pool.objectPrefabs.Count; cnt++) {
+			GUILayout.BeginHorizontal();
+			pool.objectPrefabs[cnt] = (GameObject)EditorGUILayout.ObjectField(pool.objectPrefabs[cnt],typeof(GameObject),true, GUILayout.MinWidth(100));
+			pool.amountToBuffer[cnt]= (int)EditorGUILayout.IntField(pool.amountToBuffer[cnt], GUILayout.MinWidth(50),GUILayout.MaxWidth(100));
+			if (GUILayout.Button ("-",GUILayout.ExpandWidth(false))) {
+				removeObject(cnt);
+				return;
+			}
+			GUILayout.EndHorizontal();
+		}
+		GUILayout.Space (10);
+    }
+
+	// Normal Object
+	public void addObject(){
+		pool.objectPrefabs.Add(null);
+		pool.amountToBuffer.Add (pool.defaultBufferAmount);
+	}
+    public void removeObject(int index){
+		pool.objectPrefabs.RemoveAt(index);
+		pool.amountToBuffer.RemoveAt(index);
+	}
+}
---
+++ Assets/Scripts/Util/PoolManager.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PoolManager : Singleton<PoolManager>
+{
+    public List<GameObject> objectPrefabs = new List<GameObject>();
+
+    public List<GameObject>[] pooledObjects;
+
+    public List<int> amountToBuffer = new List<int>();
+
+    public int defaultBufferAmount = 3;
+
+    protected GameObject containerObject;
+
+    public override void Awake()
+    {
+        base.Awake();
+        StartPool();
+    }
+    void StartPool()
+    {
+        containerObject = new GameObject("ObjectPool");
+
+        pooledObjects = new List<GameObject>[objectPrefabs.Count];
+
+        int i = 0;
+        foreach (GameObject objectPrefab in objectPrefabs)
+        {
+            objectPrefab.SetActive(false);
+            pooledObjects[i] = new List<GameObject>();
+
+            int bufferAmount;
+
+            if (i < amountToBuffer.Count) bufferAmount = amountToBuffer[i];
+            else
+                bufferAmount = defaultBufferAmount;
+
+            for (int n = 0; n < bufferAmount; n++)
+            {
+                GameObject newObj = Instantiate(objectPrefab) as GameObject;
+                newObj.name = objectPrefab.name;
+                Despawn(newObj);
+            }
+
+            i++;
+            objectPrefab.SetActive(true);
+        }
+    }
+    public GameObject Spawn(string objectType, bool onlyPooled)
+    {
+        for (int i = 0; i < objectPrefabs.Count; i++)
+        {
+            GameObject prefab = objectPrefabs[i];
+            if (prefab.name == objectType)
+            {
+                if (pooledObjects[i].Count > 0)
+                {
+                    GameObject pooledObject = pooledObjects[i][0];
+                    pooledObjects[i].RemoveAt(0);
+                    pooledObject.transform.parent = null;
+                    pooledObject.SetActive(true);
+
+                    return pooledObject;
+                }
+                else if (!onlyPooled)
+                {
+                    return Instantiate(objectPrefabs[i]) as GameObject;
+                }
+                break;
+            }
+        }
+        return null;
+    }
+    public GameObject Spawn(string objectType, Vector3 objectPosition, Quaternion objectRotation, bool onlyPooled)
+    {
+        for (int i = 0; i < objectPrefabs.Count; i++)
+        {
+            GameObject prefab = objectPrefabs[i];
+            if (prefab.name == objectType)
+            {
+                if (pooledObjects[i].Count > 0)
+                {
+                    GameObject pooledObject = pooledObjects[i][0];
+                    pooledObjects[i].RemoveAt(0);
+                    if (pooledObject.GetComponent<RectTransform>() != null)
+                    {
+                        pooledObject.GetComponent<RectTransform>().SetParent(containerObject.transform, false);
+                    }
+                    else
+                    {
+                        pooledObject.transform.parent = containerObject.transform;
+                    }
+                    pooledObject.SetActive(true);
+                    pooledObject.transform.position = objectPosition;
+                    pooledObject.transform.rotation = objectRotation;
+
+                    return pooledObject;
+                }
+                else if (!onlyPooled)
+                {
+                    return Instantiate(objectPrefabs[i]) as GameObject;
+                }
+                break;
+            }
+        }
+        return null;
+    }
+    public void Despawn(GameObject obj)
+    {
+        for (int i = 0; i < objectPrefabs.Count; i++)
+        {
+            if (objectPrefabs[i].name == obj.name)
+            {
+                obj.SetActive(false);
+                if (obj.GetComponent<RectTransform>() != null)
+                {
+                    obj.GetComponent<RectTransform>().SetParent(containerObject.transform, false);
+                }
+                else
+                {
+                    obj.transform.parent = containerObject.transform;
+                }
+                pooledObjects[i].Add(obj);
+                return;
+            }
+        }
+    }
+}
